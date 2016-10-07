cbb0ff7e991156f2249b77ba4d1bfad95b3c731c
Daniil Yastremskiy <Catharsis@post.cz>
2016-10-07 01:34:50 +0100

    Add tests for new API requests

--- spec/lfmAPI-spec.js
+++ spec/lfmAPI-spec.js
@@ -19,10 +19,26 @@ describe('Module for making requests to Last.fm API', function () {
             });
         });
     });
-    describe('track.getSimilar', function () {
+    describe('track.getInfo', function () {
         it('gets info about track', function (done) {
             lfmAPI.track.getInfo('Band of Sculls', "You're Not Pretty But You Got It Goin' On", function (trackInfo) {
                 expect(trackInfo.track.name).toEqual("You're Not Pretty But You Got It Goin' On");
+                done();
+            });
+        });
+    });
+    describe('downloadFullPage', function () {
+        it('downloads full Last.fm page', function (done) {
+            lfmAPI.downloadFullPage('http://www.last.fm/music/Natalie+Imbruglia/_/Torn', function (body) {
+                expect(body.length).toBeGreaterThan(0);
+                done();
+            });
+        });
+    });
+    describe('artist.getTopTracks', function () {
+        it('gets top tracks for given artist', function (done) {
+            lfmAPI.artist.getTopTracks('R.E.M.', '1', function (tracks) {
+                expect(tracks.toptracks.track[0].name).toEqual('Losing My Religion');
                 done();
             });
         });
