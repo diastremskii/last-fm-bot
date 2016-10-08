cfa5879123c16e2008c71e3d30eee94fe6e671bc
Daniil Yastremskiy <Catharsis@post.cz>
2016-10-08 01:48:10 +0100

    Refactor+fix inlineKeyboard

--- modules/telegram/telegramTypes.js
+++ modules/telegram/telegramTypes.js
@@ -6,28 +6,38 @@ tgTypes.InlineKeyboardMarkup = function (rowWidth) {
     this['inline_keyboard'] = [[]];
     var rowWidth = (rowWidth > 8 ? 8 : rowWidth) || 8;  //Currently maximum supported in one row
     //Closure to make this property private
-    this.rowWidth = function () {
+    this._rowWidth = function () {
         return rowWidth;
     };
 };
 
 tgTypes.InlineKeyboardMarkup.prototype.add = function (text, field, fieldValue) {
-    var rowCount = this['inline_keyboard'].length;
-    if (this['inline_keyboard'][rowCount-1].length === this.rowWidth()-1) {
-        this.endRow();
-    }
-    this['inline_keyboard'][rowCount-1].push(
+    this['inline_keyboard'][this._lastRow()].push(
         {
             'text': text,
             [field]: fieldValue
         }
     );
+
+    if (this._isRowFull()) {
+        this.newRow();
+    };
     return this;
 };
 
-tgTypes.InlineKeyboardMarkup.prototype.endRow = function () {
+tgTypes.InlineKeyboardMarkup.prototype.newRow = function () {
     this['inline_keyboard'].push([]);
     return this;
+};
+
+tgTypes.InlineKeyboardMarkup.prototype._isRowFull = function () {
+    if (this['inline_keyboard'][this._lastRow()] === this._rowWidth()-1) {
+        return true;
+    };
+};
+
+tgTypes.InlineKeyboardMarkup.prototype._lastRow = function () {
+    return this['inline_keyboard'].length-1;
 };
 
 module.exports = tgTypes;
