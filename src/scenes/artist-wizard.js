758ff8246dd94d3c4864010b28dea2a490d394d7
Daniil Yastremskiy <Catharsis@post.cz>
2017-07-14 14:35:55 +0300

    Add "Back" button to artist wizard

--- src/scenes/artist-wizard.js
+++ src/scenes/artist-wizard.js
@@ -1,18 +1,28 @@
 const { Markup } = require('telegraf');
 const { WizardScene } = require('telegraf-flow');
 
+const backButton = [
+  '⬅️ Back'
+];
+
+const backKeyboard = Markup
+  .keyboard(backButton)
+  .resize()
+  .extra();
+
 const artistWizard = new WizardScene('artistWizard',
   (ctx) => {
-    ctx.reply(
-      'Send me the artist name please',
-      Markup.removeKeyboard().extra()
-    );
+    ctx.reply('Send me the artist name please', backKeyboard);
     ctx.flow.wizard.next();
   },
   (ctx, next) => {
     if (ctx.message === undefined) {
       return next();
     }
+    if (ctx.message.text === '⬅️ Back') {
+      return ctx.flow.enter('startMenu');
+    }
+
     ctx.session.artist = ctx.message.text;
     ctx.flow.enter('artistMenu');
   }
